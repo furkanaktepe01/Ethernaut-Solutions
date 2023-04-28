a133c3e9955d37bb0dd627e05f41e803fae0adfc
furkankatepe01 <7.furkanaktepe@gmail.com>
2023-04-27 20:40:07 -0400

    .

---
+++ Ethernaut/Telephone/Solution.js
@@ -0,0 +1,14 @@
+/**
+
+Telephone
+ 
+Vulnerability: 
+Unreasonable Reassignment of Ownership
+
+Preventative Techniques:
+Reassign under reassignment-worthy conditions
+
+Notes:
+Beware of scenarios where tx.origin is not msg.sender
+
+*/
