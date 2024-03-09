cf33c6c13a293f23f356c8005d170afcd145a485
Furkan Aktepe <7.furkanaktepe@gmail.com>
2024-03-09 09:56:50 -0500

    .

--- Ethernaut/King/Solution.js
+++ Ethernaut/King/Solution.js
@@ -7,6 +7,6 @@ Transfers to the Attack contract revert as receive() reverts
 
 Preventative Techniques:
 Do not try to transfer directly
-Implement withdrawal pattern instead
+Implement the withdrawal pattern instead
 
 */
